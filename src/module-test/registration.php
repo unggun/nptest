9774ce331bdc1dc2ee78ca30049887ff773f11c6
Alim618 <alim.sugiharto@sirclo.com>
2022-05-19 04:16:43 +0000

    Fix & Add Test Module

---
+++ src/module-test/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © ICUBE by SIRCLO All rights reserved.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Icube_TestModule',
+    __DIR__
+);
